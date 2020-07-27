6b803b921e215df3fbac52c8c04e75fc4a64791a
Vilhelm Melkstam <vilhelm.melkstam@gmail.com>
2020-07-27 22:58:09 +0200

    Show options and timer page based on state

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,27 @@ import logo from './logo.svg';
 import './App.css';
 
 import timerMachine from './lib/stateMachine/machine';
+import OptionsPage from './pages/OptionsPage';
+import TimerPage from './pages/TimerPage';
 
 function App(): React.ReactElement {
   const [state, send] = useMachine(timerMachine);
 
+  let contents;
+  if (state.matches('timerOff')) {
+    contents = (
+      <OptionsPage
+        state={state}
+        send={send}
+      />
+    );
+  } else {
+    contents = <TimerPage />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {contents}
     </div>
   );
 }
