5d05e323abc51963b6c2f7259ac934a01a4fe17f
Vilhelm Melkstam <vilhelm.melkstam@gmail.com>
2020-08-10 18:33:39 +0200

    Add state and send to layout tests to fix them

--- src/components/Layout/Layout.spec.tsx
+++ src/components/Layout/Layout.spec.tsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Layout from './Layout';
+import { TimerState } from '../../lib/timerMachine/timerMachineDeclarations';
+
+const setupState = (): TimerState => {
+  const state: Partial<TimerState> = {
+    matches: jest.fn().mockImplementation(() => true),
+  };
+  return state as TimerState;
+};
 
 describe('<Layout />', () => {
   it('renders app name', () => {
-    const { getByText } = render(<Layout>Contents</Layout>);
+    const state = setupState();
+    const { getByText } = render(
+      <Layout
+        state={state}
+        send={jest.fn()}
+      >
+        Contents
+      </Layout>,
+    );
 
     expect(getByText('Mobber')).toBeInTheDocument();
   });
 
   it('renders children', () => {
+    const state = setupState();
     const children = <p>Children</p>;
 
-    const { getByText } = render(<Layout>{children}</Layout>);
+    const { getByText } = render(
+      <Layout
+        state={state}
+        send={jest.fn()}
+      >
+        {children}
+      </Layout>,
+    );
 
     expect(getByText('Children')).toBeInTheDocument();
   });
